6e5a60fa0a96e6f30e53bba4f385b98a885c3dba
Adrian Dvergsdal <git@atmoz.no>
2013-03-09 02:10:33 +0100

    Refactor

--- static-web.js
+++ static-web.js
@@ -1,52 +1,22 @@
 ;(function($) {
 
-    // Helper function to check suffix in URLs
-    function endsWith(str, suffix) {
-        return str.indexOf(suffix, str.length - suffix.length) !== -1;
-    }
+    window.staticWeb = function(options) {
+        var defaults = {
+            container: document,
+            contentSelector: '#content',
+            linkSelector: '#menu a',
+            loadPageFail: function($content, url) {
+                $content.html('Can\'t load ' + encodeURI(url));
+            },
+            contentHandler: function(data, url) {
+                return data;
+            }
+        };
 
-    // Load page with AJAX, converting markdown files to HTML
-    function loadPage(url, addToHistory) {
-        if (url.indexOf('#!') == -1) {
-            return false; // Without hashbang, do nothing
-        }
+        options = $.extend( {}, defaults, options );
 
-        var cleanUrl = url.replace('#!', '');
-        var $content = $('#static-content');
-
-        if (cleanUrl == '') {
-            $content.html(''); // Empty url - empty content
-        }
-        else {
-            $.get(cleanUrl, function(data) {
-                if (endsWith(cleanUrl, '.md')) {
-                    data = marked(data);
-                }
-
-                $content.html(data);
-                prepareLinks($content);
-
-                if (addToHistory) {
-                    history.pushState('', '', url);
-                }
-            });
-        }
-    }
-
-    // Add click handler on links
-    function prepareLinks(container) {
-        $(container).find('a[href$=".md"]').each(function() {
-            $(this).attr('href', '#!' + $(this).attr('href'));
-        }).click(function(event) {
-            event.preventDefault();
-            loadPage($(this).attr('href'), true);
-        });
-    }
-
-    // Get ready baby
-    $(document).ready(function() {
         // Get those links ready for action
-        prepareLinks(document);
+        prepareLinks(options.container);
 
         // Load pages when user browse the history
         $(window).bind('popstate', function(event) {
@@ -55,6 +25,41 @@
 
         // Try loading the page right now
         loadPage(location.hash, false);
-    });
+
+        // Load page with AJAX, converting markdown files to HTML
+        function loadPage(url, addToHistory) {
+            if (url.indexOf('#!') == -1) {
+                return false; // Without hashbang, do nothing
+            }
+
+            var cleanUrl = url.replace('#!', '');
+            var $content = $(options.contentSelector);
+
+            if (cleanUrl == '') {
+                $content.html(''); // Empty url - empty content
+            }
+            else {
+                $.get(cleanUrl, function(data) {
+                    data = options.contentHandler(data, cleanUrl);
+                    $content.html(data);
+                    prepareLinks($content);
+
+                    if (addToHistory) {
+                        history.pushState('', '', url);
+                    }
+                }).fail(options.loadPageFail($content, cleanUrl));
+            }
+        }
+
+        // Add click handler on links
+        function prepareLinks(container) {
+            $(container).find(options.linkSelector).each(function() {
+                $(this).attr('href', '#!' + $(this).attr('href'));
+            }).click(function(event) {
+                event.preventDefault();
+                loadPage($(this).attr('href'), true);
+            });
+        }
+    }
 
 })(jQuery);
